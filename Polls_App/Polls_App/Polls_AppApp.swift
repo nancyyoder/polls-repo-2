6096126071639443367542cf18b51bf4a2d60d0a
Nancy Yoder <nayo4039@colorado.edu>
2022-09-14 15:15:28 -0600

    first

---
+++ Polls_App/Polls_App/Polls_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Polls_AppApp.swift
+//  Polls_App
+//
+//  Created by Nancy Yoder on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Polls_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Polls_App/Polls_AppUITests/Polls_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Polls_AppUITestsLaunchTests.swift
+//  Polls_AppUITests
+//
+//  Created by Nancy Yoder on 9/14/22.
+//
+
+import XCTest
+
+class Polls_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
